c810837388b7a3dee06d80431a41cdc9b4b1edf8
Chuigda Whitegive <chuigda@doki7.club>
2025-06-14 17:33:10 +0800

    add `module-info.java`

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module club.doki7.cg112 {
+    requires club.doki7.ffm;
+    requires club.doki7.glfw;
+    requires club.doki7.vma;
+    requires club.doki7.vulkan;
+
+    requires java.logging;
+    requires org.jetbrains.annotations;
+
+    exports club.doki7.cg112.exc;
+    exports club.doki7.cg112.util;
+    exports club.doki7.cg112.vk;
+}
