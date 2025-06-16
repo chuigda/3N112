8aa767c12ec270e6a68bae7f64f189f5d1b3c7d2
Chuigda Whitegive <chuigda@doki7.club>
2025-06-17 00:49:05 +0800

    长枪策马平天下 此番诀别却为难

--- src/main/java/club/doki7/rkt/vk/pipeline/ShaderModule.java
+++ src/main/java/club/doki7/rkt/vk/pipeline/ShaderModule.java
@@ -1,13 +1,43 @@
 package club.doki7.rkt.vk.pipeline;
 
+import club.doki7.ffm.annotation.EnumType;
+import club.doki7.ffm.ptr.IntPtr;
+import club.doki7.rkt.exc.VulkanException;
 import club.doki7.rkt.vk.IDisposeOnContext;
 import club.doki7.rkt.vk.RenderContext;
+import club.doki7.vulkan.datatype.VkShaderModuleCreateInfo;
+import club.doki7.vulkan.enumtype.VkResult;
 import club.doki7.vulkan.handle.VkShaderModule;
 
+import java.lang.foreign.Arena;
 import java.lang.ref.Cleaner;
+import java.util.Objects;
 
 public final class ShaderModule implements AutoCloseable {
     public final VkShaderModule handle;
+
+    public static ShaderModule create(RenderContext cx, byte[] code) throws VulkanException {
+        assert code.length % 4 == 0 : "Shader code length must be a multiple of 4 bytes";
+
+        try (Arena arena = Arena.ofConfined()) {
+            VkShaderModuleCreateInfo createInfo = VkShaderModuleCreateInfo.allocate(arena)
+                    .pCode(IntPtr.allocate(arena, code))
+                    .codeSize(code.length);
+            VkShaderModule.Ptr pShaderModule = VkShaderModule.Ptr.allocate(arena);
+            @EnumType(VkResult.class) int result = cx.dCmd.createShaderModule(
+                    cx.device,
+                    createInfo,
+                    null,
+                    pShaderModule
+            );
+            if (result != VkResult.SUCCESS) {
+                throw new VulkanException(result, "无法创建着色器模块");
+            }
+
+            VkShaderModule handle = Objects.requireNonNull(pShaderModule.read());
+            return new ShaderModule(handle, cx);
+        }
+    }
 
     @Override
     public void close() throws Exception {
