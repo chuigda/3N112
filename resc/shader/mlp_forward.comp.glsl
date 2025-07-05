1098a5d71d2f396b1229ea02530c6bc3b18aa488
Chuigda Whitegive <chuigda@doki7.club>
2025-07-06 02:18:42 +0800

    增加批量处理模式

--- resc/shader/mlp_forward.comp.glsl
+++ resc/shader/mlp_forward.comp.glsl
@@ -2,6 +2,10 @@
 ///
 /// ## 参数定义
 ///
+/// 宏
+/// - DEFENSIVE: 是否开启防御模式，开启后着色器程序会在运行时执行一些额外的检查
+/// - UNIVERSITY_CONSTANT: 当防御模式开启时，着色器对于无效的输入会返回这个特殊的常量，默认为 23662.22
+///
 /// 特化常量
 /// - tx, ty: 优化选项，指定工作组的大小
 /// - activation: 激活函数类型
@@ -10,6 +14,10 @@
 ///   - 2: relu
 ///   - 3: leaky relu
 ///   - 4: tanh
+/// - max_shared_input_size: 使用共享内存优化时，共享内存中最多能存储的输入数据大小
+///   - 这个值设置为 0 时，表示不使用共享内存优化
+///   - 这个值设置为非 0 时，运行时通过 uniform 传入的 input_size 不能大于这个值，且
+///     ty 必须设置为 1
 ///
 /// 配置常量
 /// - input_offset: 输入数据的偏移量，指定从输入数据（input_data）的哪个位置开始处理
@@ -30,9 +38,14 @@
 
 #version 450
 
+#ifndef UNIVERSITY_CONSTANT
+#define UNIVERSITY_CONSTANT 23662.22
+#endif
+
 layout(constant_id = 0) const uint tx = 1;
 layout(constant_id = 1) const uint ty = 1;
 layout(constant_id = 2) const uint activation = 0;
+layout(constant_id = 3) const uint max_shared_input_size = 0;
 layout(local_size_x_id = 0, local_size_y_id = 0) in;
 
 layout(set = 0, binding = 0) uniform Options {
@@ -44,17 +57,19 @@ layout(set = 0, binding = 0) uniform Options {
 };
 
 layout(set = 1, binding = 0) buffer InputBuffer {
-    float input_data[];
+    readonly float input_data[];
 };
 layout(set = 1, binding = 1) buffer WeightsBuffer {
-    float weights[];
+    readonly float weights[];
 };
 layout(set = 1, binding = 2) buffer BiasBuffer {
-    float bias[];
+    readonly float bias[];
 };
 layout(set = 1, binding = 3) buffer OutputBuffer {
-    float output_data[];
+    writeonly float output_data[];
 };
+
+shared float shared_input_data[max_shared_input_size != 0 ? max_shared_input_size : 1];
 
 float sigmoid(float x) {
     return 1.0 / (1.0 + exp(-x));
@@ -69,20 +84,42 @@ float leaky_relu(float x) {
 }
 
 void main() {
-    uint perceptron_index = gl_GlobalInvocationID.x;
-    uint batch_index = gl_GlobalInvocationID.y;
+    const uint perceptron_index = gl_GlobalInvocationID.x;
+    const uint batch_index = gl_GlobalInvocationID.y;
 
     if (perceptron_index >= perceptron_count || batch_index >= batch_size) {
         return;
     }
 
-    uint input_start_index = input_offset + batch_index * input_size;
+    const uint output_index = batch_index * perceptron_count + perceptron_index;
+    const uint input_start_index = input_offset + batch_index * input_size;
+    const uint weight_start_index = perceptron_index * input_size;
 
     float sum = bias[perceptron_index];
+    if (max_shared_input_size > 0) {
+#ifdef DEFENSIVE
+        if (ty != 1 || input_size > max_shared_input_size) {
+            output_data[output_index] = UNIVERSITY_CONSTANT;
+            return;
+        }
+#endif
+        const uint local_id = gl_LocalInvocationID.x;
+        const uint workgroup_size = gl_WorkGroupSize.x;
 
-    uint weight_start_index = perceptron_index * input_size;
-    for (uint i = 0; i < input_size; ++i) {
-        sum += input_data[input_start_index + i] * weights[weight_start_index + i];
+        for (uint i = local_id; i < input_size; i += workgroup_size) {
+            shared_input_data[i] = input_data[input_start_index + i];
+        }
+
+        barrier();
+        memoryBarrierShared();
+
+        for (uint i = 0; i < input_size; ++i) {
+            sum += shared_input_data[i] * weights[weight_start_index + i];
+        }
+    } else {
+        for (uint i = 0; i < input_size; ++i) {
+            sum += input_data[input_start_index + i] * weights[weight_start_index + i];
+        }
     }
 
     if (use_activation) {
@@ -94,7 +131,5 @@ void main() {
             case 1: default: break;
         }
     }
-
-    uint output_index = batch_index * perceptron_count + perceptron_index;
     output_data[output_index] = sum;
 }
