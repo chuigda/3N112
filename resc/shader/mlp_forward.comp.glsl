89b0d772e0e6f6d1f0d4ac469da968523b4f41eb
Chuigda Whitegive <chuigda@doki7.club>
2025-07-06 00:43:47 +0800

    听见风，当成雨，到处去作妖

--- resc/shader/mlp_forward.comp.glsl
+++ resc/shader/mlp_forward.comp.glsl
@@ -1,10 +1,18 @@
 /// 多层感知机（MLP）前向传播算法
 ///
-/// 特化常量：
+/// ## 参数定义
+///
+/// 特化常量
 /// - tx, ty: 优化选项，指定工作组的大小
+/// - transposed_weights: 权重矩阵是否有转置
+///
+/// Dispatch
+/// - WorkGroupSize.y * WorkGroupCount.y: 批次数量
+/// - WorkGroupSize.x * WorkGroupCount.x: 感知机数量
 ///
 /// 配置常量
 /// - input_offset: 输入数据的偏移量，指定从输入数据（input_data）的哪个位置开始处理
+/// - perceptron_count: 感知机的数量，同时也是对批次内每组数据输出的数据数量
 /// - input_size: 感知机接受的输入尺寸
 /// - use_activation: 本层是否使用激活函数
 ///
@@ -20,10 +28,12 @@
 
 layout(constant_id = 0) const uint tx = 1;
 layout(constant_id = 1) const uint ty = 1;
+layout(constant_id = 2) const bool transposed_weights = false;
 layout(local_size_x_id = 0, local_size_y_id = 0) in;
 
 layout(set = 0, binding = 0) uniform Options {
     uint input_offset;
+    uint perceptron_count;
     uint input_size;
     bool use_activation;
 };
@@ -46,21 +56,29 @@ float sigmoid(float x) {
 }
 
 void main() {
-    uint perceptron_count = gl_WorkGroupSize.x * gl_NumWorkGroups.x;
     uint batch_size = gl_WorkGroupSize.y * gl_NumWorkGroups.y;
 
     uint perceptron_index = gl_GlobalInvocationID.x;
     uint batch_index = gl_GlobalInvocationID.y;
 
-    uint input_start_index = input_offset + batch_index * input_size;
-    uint weight_start_index = perceptron_index * input_size;
-
-    float sum = 0.0;
-    for (uint i = 0; i < input_size; ++i) {
-        sum += input_data[input_start_index + i] * weights[weight_start_index + i];
+    if (perceptron_index >= perceptron_count || batch_index >= batch_size) {
+        return;
     }
 
-    sum += bias[perceptron_index];
+    uint input_start_index = input_offset + batch_index * input_size;
+
+    float sum = bias[perceptron_index];
+
+    if (transposed_weights) {
+        for (uint i = 0; i < input_size; ++i) {
+            sum += input_data[input_start_index + i] * weights[perceptron_index + i * perceptron_count];
+        }
+    } else {
+        uint weight_start_index = perceptron_index * input_size;
+        for (uint i = 0; i < input_size; ++i) {
+            sum += input_data[input_start_index + i] * weights[weight_start_index + i];
+        }
+    }
 
     if (use_activation) {
         sum = sigmoid(sum);
